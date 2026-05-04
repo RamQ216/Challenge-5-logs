6f04e45f1a24e3ff4538245bc1d847f46ddf3e5a
RamQ216 <hugomramirez06@gmail.com>
2026-05-04 16:38:34 -0300

    fea:implementacion de comentarios breves

--- SERVICIOS/servicio1.py
+++ SERVICIOS/servicio1.py
@@ -1,21 +1,21 @@
 from estructura import LogSimulator
 
-class GestorIdentidad(LogSimulator):
-    def __init__(self, ruta, token):
+class GestorIdentidad(LogSimulator):#generamos la clase hija
+    def __init__(self, ruta, token):#definimos el token y ruta
         # Llamamos al constructor del padre
         super().__init__(ruta, "gestor-identidad")
 
-        self.sesion_abierta.headers.update({
+        self.sesion_abierta.headers.update({#generamos el titulo de autorizacion
             "Authorization": f"Bearer {token}"
         })
-        self.eventos_disponibles = [
+        self.eventos_disponibles = [#indicamos los eventos posibles con su nivel referente a cada servicio
             {"nivel": "INFO", "mensaje": "Usuario inició sesión"},
             {"nivel": "WARNING", "mensaje": "Intento de login fallido"},
             {"nivel": "CRITICAL", "mensaje": "Ataque por fuerza bruta detectado"},
             {"nivel": "INFO", "mensaje": "Cierre de sesión exitoso"}
         ]
 
-if __name__ == "__main__":
+if __name__ == "__main__":#ejecutamos el script
     URL = "http://127.0.0.1:5000/logs"
-    app = GestorIdentidad(URL,"token-identidad")
-    app.simulacion()
+    app = GestorIdentidad(URL,"token-identidad")#intanciamos la clase
+    app.simulacion()#llamamos al metodo que envia los logs
